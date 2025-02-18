d35a3d451b6b741dfc67352dc164484b02701704
seung1177 <tmddnekdy@gmail.com>
2025-02-18 19:23:07 +0900

    코드정리

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ftp = require('basic-ftp');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
@@ -8,22 +9,41 @@ app.get('*', async (req, res) => {
   const client = new ftp.Client();
   try {
     await client.access({
-      host: '255.255.255.255',
+      host: '127.0.0.1',
       user: 'User',
       password: 'Password',
     });
 
     const remoteFilePath = req.originalUrl;
-    const localFilePath = path.join(__dirname, 'revfiles', path.basename(remoteFilePath)); // 다운로드한 파일을 저장할 경로
+    const localFilePath = path.join(__dirname, 'ftpfiles', path.basename(remoteFilePath));
 
-    await client.downloadTo(localFilePath, remoteFilePath); // 파일 다운로드
+    // 파일 존재 여부 확인
+    try {
+      await client.size(remoteFilePath);
+    } catch (err) {
+      console.error(`File not found: ${remoteFilePath}`);
+      return res.status(404).send('File not found');
+    }
 
-    res.sendFile(localFilePath); // 다운로드한 파일을 클라이언트에게 전송
+    await client.downloadTo(localFilePath, remoteFilePath);
+    res.sendFile(localFilePath, (err) => {
+      if (err) {
+        console.error(`Error sending file: ${err}`);
+      }
+
+      // 일정 시간 후 파일 삭제 (예: 10초 후)
+      setTimeout(() => {
+        fs.unlink(localFilePath, (err) => {
+          if (err) console.error(`Error deleting file: ${err}`);
+          else console.log(`Deleted file: ${localFilePath}`);
+        });
+      }, 10000);
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
   }
-  client.close(); // FTP 서버 연결 종료
+  client.close();
 });
 
 const PORT = process.env.PORT || 9000;
